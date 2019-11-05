be10f737c88b3f77c581d409c8580daa7c9825d4
Jeremy <JeremyPlott@gmail.com>
2019-11-05 16:06:20 -0500

    works, but times out on some tests

--- HRClimbingTheLeaderboard/Program.cs
+++ HRClimbingTheLeaderboard/Program.cs
@@ -1,9 +1,64 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 namespace HRClimbingTheLeaderboard {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            int[] scores = { 50, 100, 50, 40, 40, 20, 10 };
+            int[] alice = { 5, 25, 50, 120 };
+
+            List<int> leaderboard = new List<int>();
+            List<int> placementarr = new List<int>();
+
+            foreach(var score in scores) {
+                if(!leaderboard.Contains(score)) {
+                    leaderboard.Add(score);
+                }               
+            }
+
+            leaderboard.Sort();
+
+            foreach(var ascore in alice) {
+
+                if(ascore > leaderboard.Max()) { placementarr.Add(1); continue; }
+                if(ascore < leaderboard.Min()) { placementarr.Add(leaderboard.Count + 1); continue; }
+
+                var rank = 0;
+
+                foreach (var lbscore in leaderboard) {
+
+                    var placement = leaderboard.Count() - rank + 1;
+                    var equalplacement = leaderboard.Count() - rank;
+
+                    if(ascore == lbscore) {
+                        placementarr.Add(equalplacement);
+                        break;
+                    }
+                    if(ascore > lbscore) {
+                        rank++;
+                        continue;
+                    } else {
+                        placementarr.Add(placement);
+                        break;
+                    }
+                }
+            }
+            ;
+            foreach(var p in placementarr.ToArray()) {
+                Console.WriteLine(p);
+            }
         }
     }
 }
